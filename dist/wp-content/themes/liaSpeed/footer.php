43b004a40642c0d9ef03ee12b0d44613c566bf1b
Minh Viet <vietngm@gmail.com>
2025-05-14 16:53:13 +0700

    Update footer.php
    Update

--- dist/wp-content/themes/liaSpeed/footer.php
+++ dist/wp-content/themes/liaSpeed/footer.php
@@ -1,15 +1,3 @@
-<?php
-  $post_branchs = get_posts(array(
-		"post_type" => "branch",
-		"posts_per_page" => -1,
-	));
-
-?>
-
-<?php foreach ($post_branchs as $branch) : ?>
-<?php echo json_encode(get_field('chuyen_vien',$branch->ID));?>
-<?php endforeach; ?>
-
 <?php get_template_part( 'template-parts/footer', "menu"); ?>
 <?php get_template_part( 'template-parts/footer', "nav"); ?>
 <?php get_template_part('template-parts/modal','consultation-success'); ?>
