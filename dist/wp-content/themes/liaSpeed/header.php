1f4ce1e2a66aa7fae0873136b5fe119abfdd26c8
Minh Viet <vietngm@gmail.com>
2025-05-30 16:56:17 +0700

    Update
    Update

--- dist/wp-content/themes/liaSpeed/header.php
+++ dist/wp-content/themes/liaSpeed/header.php
@@ -46,30 +46,10 @@
   <body>
 
     <header class="sticky top-0 left-0 right-0 bg-white z-50 shadow-sm">
-      <div class="h-[60px] " style="z-index:100" id="ad-container">
-        <div
-          class="flex border-1 items-center bg-white border border-gray-300 rounded-lg p-2 shadow-md max-w-sm relative gap-2">
-          <button onclick="document.getElementById('ad-container').remove()"
-            class=" left-2 top-1 text-gray-500 hover:text-gray-800 text-lg">
-            <img class="w-4 h-4" src="<?= get_theme_file_uri("assets/images/icons/close-gray.svg") ?>" alt="" />
-          </button>
-
-          <img class="h-10.5" src="<?= get_theme_file_uri("assets/images/logo.png") ?>"
-            class="w-10 h-10 rounded-md ml-5">
-          <div class="ml-3 flex-1">
-            <div class="text-sm font-semibold text-gray-900">LiA - Kết nối & Chia sẻ</div>
-            <p class="text-xs text-gray-600" style="font-size:10px">Ứng dụng một nền tảng công nghệ!</p>
-          </div>
-          <a id="downloadApp" style="    border-radius: 24px;
-						padding: 4px 12px;
-						background: #1a5478;
-						color: #FFF;
-						font-size: 12px;" class="bg-blue-500 text-xs font-semibold px-3 py-1 rounded-lg hover:bg-blue-600">
-            Tải app
-          </a>
-        </div>
-      </div>
-      <?php //include get_template_directory()."/content/header-bk.php"; ?>
+      <?php include get_template_directory()."/content/app-download.php"; ?>
+      <?php      	
+       if( !is_page('danh-sach-chuyen-vien')) include get_template_directory()."/content/header.php";
+      ?>
     </header>
 
     <div id="chat-modal" data-open="false"
