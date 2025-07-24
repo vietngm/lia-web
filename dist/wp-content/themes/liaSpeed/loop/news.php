86798a3685a8f4317b08a23080b2d8be7683f9fd
Minh Viet <vietngm@gmail.com>
2025-07-24 14:52:45 +0700

    update
    Update

--- dist/wp-content/themes/liaSpeed/loop/news.php
+++ dist/wp-content/themes/liaSpeed/loop/news.php
@@ -1,15 +1,16 @@
 <?php
-  $thumb = get_field('anh_dai_dien', $post->ID);
+  // $thumb = get_field('anh_dai_dien', $post->ID);
+  $thumb = get_the_post_thumbnail_url(get_the_ID(),'full');
 ?>
 <a href="<?php echo get_permalink($post->ID); ?>" class='news-link'>
   <?php if ($thumb) { ?>
   <div class='news-thumb'>
-    <img class="img aspect-square lazy" src="<?php echo $thumb['url'] ?>" alt="<?php echo $post->post_title; ?>">
+    <img class="img lazy" src="<?php echo $thumb; ?>" alt="<?php echo $post->post_title; ?>">
   </div>
   <?php } else { ?>
   <div class='news-noimage'>
-    <img class="img aspect-square lazy" src="<?php echo get_theme_file_uri('assets/images/noimg64.png'); ?>" width="64"
-      height="64" alt="<?php echo $post->post_title; ?>">
+    <img class="img lazy" src="<?php echo get_theme_file_uri('assets/images/noimg64.png'); ?>" width="64" height="64"
+      alt="<?php echo $post->post_title; ?>">
   </div>
   <?php } ?>
   <div class="news-detail">
--- dist/wp-content/themes/liaSpeed/single-tin-tuc.php
+++ dist/wp-content/themes/liaSpeed/single-tin-tuc.php
@@ -1,20 +1,29 @@
 <?php get_header(); ?>
 <?php $fields = get_fields(); ?>
 <main class="is-news">
-  <section class="section-booking-banner w-full h-[200px] relative px-4 flex items-center justify-center">
-    <?php $image = get_the_post_thumbnail_url( ) ?>
-    <img class="w-full h-full object-cover object-center absolute"
-      src="<?= $image ? $image : get_theme_file_uri("assets/images/default-banner.jpg") ?>" />
-    <div class="w-full h-full absolute bg-black bg-opacity-50"></div>
-    <h1 class="relative z-20 text-white text-24 text-center font-bold uppercase"><?= get_the_title(); ?></h1>
-  </section>
-  <section>
+  <section class="section section-blog">
     <div class="container">
-      <h2 class="form-title text-lg font-semibold border-l-4 border-purple-500 pl-2 mt-4"
+      <div class="flex gap-2 flex-wrap mb-4">
+        <a class="text-primary font-semibold" href="/">Trang chủ</a>
+        <span>›</span>
+        <a class="text-primary font-semibold" href="/tin-tuc">Tin tức & Sự kiện</a>
+        <span>›</span>
+        <a class="" href="<?= the_permalink() ?>">Chi tiết tin tức</a>
+      </div>
+      <h2 class="form-title text-lg font-semibold border-l-4 border-purple-500 pl-2"
         style="font-size:16px;color:#1A5477"><?= get_the_title(); ?></h2>
       <article>
         <?php while (have_posts()): the_post(); ?>
         <div class="article-body">
+          <?php $thumb = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
+          <div class="news-thumb-detail rounded overflow-hidden mb-4">
+            <?php if ($thumb) { ?>
+            <img class="img lazy" src="<?php echo $thumb; ?>" alt="<?php echo $post->post_title; ?>">
+            <?php } else { ?>
+            <img class="img lazy" src="<?php echo get_theme_file_uri('assets/images/noimg64.png'); ?>" width="64"
+              height="64" alt="<?php echo $post->post_title; ?>">
+            <?php } ?>
+          </div>
           <?php the_content(); ?>
         </div>
         <?php endwhile;
